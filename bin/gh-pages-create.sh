e03858024a46b3f866c0bfc4eaab0b21cbf2f199
Ramón Rial <rrialq@gmail.com>
2022-12-22 10:28:53 +0100

    Update.

--- bin/gh-pages-create.sh
+++ bin/gh-pages-create.sh
@@ -2,24 +2,14 @@
 
 BASEDIR=$( readlink -f "$( dirname "$0" )" )
 TARGET_PATH=$( readlink -f "${BASEDIR}/../target" )
+GH_PAGES_PATH=${TARGET_PATH}/gh-pages
 SITE_PATH=${TARGET_PATH}/site
 STAGING_PATH=${TARGET_PATH}/staging
-GH_PAGES_PATH=${TARGET_PATH}/gh-pages
-PROJECT_VERSION_FILE=${TARGET_PATH}/project.version
 
-if [ ! -d "${SITE_PATH}" ]; then
-    echo 'Xerando o site'
-    mvn -q site
-fi
+echo "Borrando directorios 'target/gh-pages', 'target/site' e 'target/staging'"
+[ -d "${GH_PAGES_PATH}" ] && rm -Rf "${GH_PAGES_PATH}"
+[ -d "${SITE_PATH}" ] && rm -Rf "${SITE_PATH}"
+[ -d "${STAGING_PATH}" ] && rm -Rf "${STAGING_PATH}"
 
-if [ ! -d "${STAGING_PATH}" ]; then
-    echo 'Generating staging'
-    mkdir -p "${STAGING_PATH}"
-    mvn -q site:stage
-fi
-
-mvn -q help:evaluate -D'expression=project.version' -DforceStdout -D"output=${PROJECT_VERSION_FILE}"
-PROJECT_VERSION=$( cat "${PROJECT_VERSION_FILE}" )
-echo PROJECT_VERSION = "${PROJECT_VERSION}"
-
-cp -R "${STAGING_PATH}" "${GH_PAGES_PATH}/${PROJECT_VERSION}"
+echo 'Xerando gh-pages'
+mvn -q site site:stage && mvn -q post-site
