917da99e76c424df88e0d8ef9cf89bc12efb328f
Ramón Rial <rrialq@gmail.com>
2022-12-29 13:27:47 +0100

    xxxxx

--- bin/prepare-local-repo.sh
+++ bin/prepare-local-repo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 BASEDIR=$( readlink -f "$( dirname "$0" )" )
 TARGET_PATH=$( readlink -f "${BASEDIR}/../target" )
-EXTENSION_LIB_PATH=${TARGET_PATH}/ext-lib
+EXTENSION_LIB_PATH=/usr/java/packages/lib/ext
 
 JAVA_GALICIAN_FILENAME="${EXTENSION_LIB_PATH}/javagalician-java6.jar"
 
@@ -12,7 +12,6 @@ fi
 
 if [ ! -f ${JAVA_GALICIAN_FILENAME} ]; then
     wget --quiet -O ${JAVA_GALICIAN_FILENAME} https://github.com/javagalician/javagalician-java6/releases/download/javagalician-java6-1.1/javagalician-java6-1.1.jar \
-    && vp ${JAVA_GALICIAN_FILENAME} ${EXTENSION_LIB_PATH}
 fi
 
 echo -------------
