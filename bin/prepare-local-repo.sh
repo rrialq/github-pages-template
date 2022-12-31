52c892933187c2937d63e1118d9569f26b13cbb7
Ramón Rial <rrialq@gmail.com>
2022-12-31 12:35:20 +0100

    Add cache configuration experimental.

--- bin/prepare-local-repo.sh
+++ bin/prepare-local-repo.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
+# $1 = Target file
+# $2 = URL
+downloadExtensionLib() {
+    if [ ! -f "${1}" ]; then
+        sudo wget --quiet -O "${EXTENSION_LIB_PATH}/${1}" "${2}" \
+        && echo "[  OK   ] Downloading ${1}" \
+        || echo "[ ERROR ] Downloading ${1}"
+    fi
+}
+
 BASEDIR=$( readlink -f "$( dirname "$0" )" )
 TARGET_PATH=$( readlink -f "${BASEDIR}/../target" )
 EXTENSION_LIB_PATH=/usr/java/packages/lib/ext
 
-JAVA_GALICIAN_FILENAME="${EXTENSION_LIB_PATH}/javagalician-java6.jar"
-
 if [ ! -d ${EXTENSION_LIB_PATH} ]; then
-    sudo mkdir -p ${EXTENSION_LIB_PATH}
-    sudo cp -r ${JAVA_HOME}/jre/lib/ext/* ${EXTENSION_LIB_PATH}
+    sudo mkdir -p "${EXTENSION_LIB_PATH}"
 fi
 
-if [ ! -f ${JAVA_GALICIAN_FILENAME} ]; then
-    sudo wget --quiet -O ${JAVA_GALICIAN_FILENAME} https://github.com/javagalician/javagalician-java6/releases/download/javagalician-java6-1.1/javagalician-java6-1.1.jar
-fi
-
-echo -------------
-echo java.ext.dirs
-echo -------------
-
-mvn help:evaluate -Dexpression=java.ext.dirs
+downloadExtensionLib "javagalician-java6.jar" "https://github.com/javagalician/javagalician-java6/releases/download/javagalician-java6-1.1/javagalician-java6-1.1.jar"
